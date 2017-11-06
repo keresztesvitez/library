608ce470d4e9eb4d9833a202e02765eceade2f23
Keresztes Adam <keresztesvitez@gmail.com>
2017-11-06 09:02:54 +0100

    Added BookService tests

--- src/test/java/com/epam/library/book/BookServiceTest.java
+++ src/test/java/com/epam/library/book/BookServiceTest.java
@@ -19,6 +19,9 @@ public class BookServiceTest { //TODO Crerate test database
 
     private static final long BOOK_ID_1 = 1L;
     private static final long BOOK_ID_2 = 2L;
+    private static final long BOOK_ID_3 = 3L;
+    private static final long BOOK_ID_4 = 4L;
+    private static final long BOOK_ID_5 = 5L;
     private static final long USER_ID_1 = 1L;
     private static final long USER_ID_2 = 2L;
     private static final int BORROW_DAYS = 30;
@@ -28,18 +31,44 @@ public class BookServiceTest { //TODO Crerate test database
 
     @Test
     public void testShouldBorrowBook() {
-        Borrow borrow = service.borrow(BOOK_ID_2, USER_ID_2);
+        Borrow borrow = service.borrow(BOOK_ID_3, USER_ID_2);
 
         assertThat(borrow, notNullValue());
-//        assertThat(borrow.getUser().getId(), is(USER_ID_2));
-        assertThat(borrow.getBook().getId(), is(BOOK_ID_2));
+        assertThat(borrow.getUser().getId(), is(USER_ID_2));
+        assertThat(borrow.getBook().getId(), is(BOOK_ID_3));
         assertThat(borrow.getExpiration(), is(LocalDate.now().plusDays(BORROW_DAYS)));
-
     }
 
     @Test(expected = RuntimeException.class)
-    public void testShouldThrowException() {
+    public void bookIsCurrentlyBorrowed() {
         Borrow borrow = service.borrow(BOOK_ID_1, USER_ID_2);
+    }
+
+    @Test
+    public void testShouldExtendBorrow() {
+        service.borrow(BOOK_ID_4, USER_ID_2);
+        Borrow extendedBorrow = service.extend(BOOK_ID_4, USER_ID_2);
+
+        assertThat(extendedBorrow, notNullValue());
+        assertThat(extendedBorrow.getUser().getId(), is(USER_ID_2));
+        assertThat(extendedBorrow.getBook().getId(), is(BOOK_ID_4));
+        assertThat(extendedBorrow.getExpiration(), is(LocalDate.now().plusDays(BORROW_DAYS + BORROW_DAYS)));
+        assertThat(extendedBorrow.getExtended(), is(true));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void tryToExtendANotBorrowedBook() {
+        Borrow borrow = service.extend(BOOK_ID_5, USER_ID_1);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void tryToExtendSomebodyElsesBook() {
+        Borrow borrow = service.extend(BOOK_ID_4, USER_ID_1);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void tryToExtendSecondTime() {
+        Borrow borrow = service.extend(BOOK_ID_4, USER_ID_2);
     }
 
 }
